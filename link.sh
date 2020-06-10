4471e2072f217f51df2019735aba782623b66036
Christopher Griffith <christopher.griffith@taillight.com>
2020-06-10 11:00:09 -0500

    Added coc config + link

--- link.sh
+++ link.sh
@@ -4,3 +4,4 @@ ln -s ./.tmux.conf ~/.tmux.conf
 ln -s ./.vimrc ~/.vimrc
 ln -s ./.vimrc ~/.config/nvim/init.vim
 ln -s ./.zshrc ~/.zshrc
+ln -s ./.config/nvim/coc-settings.json ~/.config/nvim/coc-settings.json
