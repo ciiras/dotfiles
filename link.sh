722a4571f6652c4b5d670b4bbf4b415f50007ec9
Christopher Griffith <christopher.griffith@taillight.com>
2020-06-10 10:59:48 -0500

    Added init.vim link for nvim

--- link.sh
+++ link.sh
@@ -2,4 +2,5 @@ ln -s ./.gitconfig ~/.gitconfig
 ln -s ./.gitignore ~/.gitignore
 ln -s ./.tmux.conf ~/.tmux.conf
 ln -s ./.vimrc ~/.vimrc
+ln -s ./.vimrc ~/.config/nvim/init.vim
 ln -s ./.zshrc ~/.zshrc
