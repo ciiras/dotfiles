f856ef29fc17138088c85450b38b8473505eb8b9
Christopher Griffith <christopher.griffith@taillight.com>
2020-06-10 16:27:23 -0500

    Updated link.sh with absolute path

--- link.sh
+++ link.sh
@@ -1,7 +1,9 @@
-ln -s ./.gitconfig ~/.gitconfig
-ln -s ./.gitignore ~/.gitignore
-ln -s ./.tmux.conf ~/.tmux.conf
-ln -s ./.vimrc ~/.vimrc
-ln -s ./.vimrc ~/.config/nvim/init.vim
-ln -s ./.zshrc ~/.zshrc
-ln -s ./.config/nvim/coc-settings.json ~/.config/nvim/coc-settings.json
+ls -s ~/.vim/ ~/.config/nvim/
+
+ln -s ~/projects/dotfiles/.config/nvim/coc-settings.json ~/.vim/coc-settings.json
+ln -s ~/projects/dotfiles/.gitconfig ~/.gitconfig
+ln -s ~/projects/dotfiles/.gitignore ~/.gitignore
+ln -s ~/projects/dotfiles/.tmux.conf ~/.tmux.conf
+ln -s ~/projects/dotfiles/.vimrc ~/.vimrc
+ln -s ~/projects/dotfiles/.vimrc ~/.config/nvim/init.vim
+ln -s ~/projects/dotfiles/.zshrc ~/.zshrc
