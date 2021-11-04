8c72a6d85d11be5bed46ff3347f6141d2e540a0f
Christopher Griffith <christopher.griffith@taillight.com>
2021-11-04 08:47:56 -0500

    adds force flag on sym link creation

--- link.sh
+++ link.sh
@@ -1,11 +1,11 @@
 curDir=$(pwd)
 
-ls -sF ~/.vim/ ~/.config/nvim/
-ln -sF ${curDir}/.vimrc ~/.config/nvim/init.vim
-ln -sF ${curDir}/.config/nvim/coc-settings.json ~/.config/nvim/coc-settings.json
+ls -sfF ~/.vim/ ~/.config/nvim/
+ln -sfF ${curDir}/.vimrc ~/.config/nvim/init.vim
+ln -sfF ${curDir}/.config/nvim/coc-settings.json ~/.config/nvim/coc-settings.json
 
-ln -sF ${curDir}/.gitconfig ~/.gitconfig
-ln -sF ${curDir}/.gitignore ~/.gitignore
-ln -sF ${curDir}/.tmux.conf ~/.tmux.conf
-ln -sF ${curDir}/.vimrc ~/.vimrc
-ln -sF ${curDir}/.zshrc ~/.zshrc
+ln -sfF ${curDir}/.gitconfig ~/.gitconfig
+ln -sfF ${curDir}/.gitignore ~/.gitignore
+ln -sfF ${curDir}/.tmux.conf ~/.tmux.conf
+ln -sfF ${curDir}/.vimrc ~/.vimrc
+ln -sfF ${curDir}/.zshrc ~/.zshrc
