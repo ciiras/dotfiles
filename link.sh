dff1c1eb0172b16965238898016da682b48494db
Christopher Griffith <christopher.griffith@taillight.com>
2021-11-04 11:08:28 -0500

    adds kitty config

--- link.sh
+++ link.sh
@@ -9,3 +9,4 @@ ln -sfF ${curDir}/.gitignore ~/.gitignore
 ln -sfF ${curDir}/.tmux.conf ~/.tmux.conf
 ln -sfF ${curDir}/.vimrc ~/.vimrc
 ln -sfF ${curDir}/.zshrc ~/.zshrc
+ln -sfF ${curDir}/kitty.conf ~/.config/kitty/kitty.conf
