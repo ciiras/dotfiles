a5d9d3b1a4459049df053a033705d9959cf4a300
Christopher Griffith <christopher.griffith@taillight.com>
2021-11-08 09:24:50 -0600

    adds always show hidden files defaults to Finder

--- osx.sh
+++ osx.sh
@@ -1,3 +1,6 @@
 # Keyboard Repeat
 defaults write -g InitialKeyRepeat -int 15 # normal minimum is 15 (225 ms)
 defaults write -g KeyRepeat -int 1 # normal minimum is 2 (30 ms)
+
+# Finder
+defaults write com.apple.finder AppleShowAllFiles -boolean true; killall Finder;
