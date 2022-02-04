7685017724bebee48b2f184353b8ba25331ab418
Christopher Griffith <christopher.griffith@taillight.com>
2022-02-04 09:19:08 -0600

    changes screenshot directory

--- osx.sh
+++ osx.sh
@@ -4,3 +4,6 @@ defaults write -g KeyRepeat -int 1 # normal minimum is 2 (30 ms)
 
 # Finder
 defaults write com.apple.finder AppleShowAllFiles -boolean true; killall Finder;
+
+# Screenshots Location
+defaults write com.apple.screencapture location /Users/griffithc/Downloads
