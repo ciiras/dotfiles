0ae34d08e99d4ff9b502446a725c3eb771cabdd3
Christopher Griffith <christopher.griffith@taillight.com>
2022-08-12 13:52:13 -0500

    updates inc search and search background color to orange

--- init.lua
+++ init.lua
@@ -344,8 +344,9 @@ require('nightfox').setup({
     },
     groups = {
         all = {
+            IncSearch = { fg ='#393b44', bg = '#F4A261' },
             NormalNC = { bg = '#303030' },
-            Search = { fg ='#393b44', bg = '#63CDCF' },
+            Search = { fg ='#393b44', bg = '#F4A261' },
             CursorLine = { bg = '#1C1C1C' },
             Substitute = { fg ='#FFFFFF' },
         },
