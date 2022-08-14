69ac37e462dcd17c372237cb21a2225f7be59bf6
Christopher Griffith <christopher.griffith@taillight.com>
2022-08-13 21:00:22 -0500

    moves options into lua/options.lua

--- init.lua
+++ init.lua
@@ -40,57 +40,7 @@ end
 
 -- Lua }}}
 
--- Settings {{{
-
-global.mapleader = ','
-global.netrw_liststyle = 3
-global.session_autosave = 'no'
-global.tmux_navigator_preserve_zoom = 1
-
-opt.autoread = true
-opt.backspace = 'indent,eol,start'
-opt.backup = false
-opt.clipboard = 'unnamedplus'
-opt.cmdheight = 2
-opt.colorcolumn = '80'
-opt.compatible = false
-opt.encoding = 'UTF-8'
-opt.expandtab = true
-opt.hidden = true
-opt.history = 100
-opt.hlsearch = true
-opt.ignorecase = true
-opt.incsearch = true
-opt.laststatus = 2
-opt.lazyredraw = true
-opt.list = true
-opt.listchars = {tab = '›~', eol = '¬', space = '⋅'}
-opt.mouse = 'a'
-opt.number = true
-opt.relativenumber = true
-opt.ruler = true
-opt.scl = 'yes'
-opt.shiftwidth = 4
-opt.shortmess:append('c')
-opt.shortmess:append('I')
-opt.showcmd = true
-opt.showmatch = true
-opt.smartcase = true
-opt.smartindent = true
-opt.splitbelow = true
-opt.splitright = true
-opt.swapfile = false
-opt.tabstop = 4
-opt.timeout = true
-opt.timeoutlen = 1000
-opt.ttimeoutlen = 0
-opt.undodir = os.getenv('HOME') .. '/.config/nvim/undodir'
-opt.undofile = true
-opt.updatetime = 300
-opt.visualbell = true
-opt.wrap = false
-
--- Settings }}}
+require('options')
 
 -- Plugins {{{
 
