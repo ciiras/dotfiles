51a291607cf8b8794b4a3580fa822d86df692cf7
Christopher Griffith <christopher.griffith@taillight.com>
2022-09-05 10:33:01 -0500

    changes trouble mapping open to toggle

--- after/plugin/trouble.lua
+++ after/plugin/trouble.lua
@@ -1,8 +1,7 @@
 local keymap = vim.api.nvim_set_keymap
 local opts = { silent = true, noremap = true }
 
-keymap('n', '<leader>xx', '<cmd>Trouble<cr>', opts)
-keymap('n', '<leader>xc', '<cmd>TroubleClose<cr>', opts)
+keymap('n', '<leader>xx', '<cmd>TroubleToggle<cr>', opts)
 keymap('n', '<leader>xw', '<cmd>Trouble workspace_diagnostics<cr>', opts)
 keymap('n', '<leader>xd', '<cmd>Trouble document_diagnostics<cr>', opts)
 keymap('n', '<leader>xl', '<cmd>Trouble loclist<cr>', opts)
