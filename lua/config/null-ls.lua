e27ea01200d1bc0b49b2d407344a77d4d0069797
Christopher Griffith <christopher.griffith@taillight.com>
2022-09-09 08:46:40 -0500

    removes codespell null-ls builtin due to perf

--- lua/config/null-ls.lua
+++ lua/config/null-ls.lua
@@ -2,9 +2,8 @@ local null_ls = require("null-ls")
 
 null_ls.setup({
     sources = {
-        null_ls.builtins.diagnostics.codespell,
+        null_ls.builtins.completion.spell,
         null_ls.builtins.diagnostics.cspell,
         null_ls.builtins.formatting.prettierd,
-        null_ls.builtins.completion.spell,
     }
 })
