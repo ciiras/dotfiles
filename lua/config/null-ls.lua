1521fa8a277647445c3a42fb347846e0ae50aacf
Christopher Griffith <christopher.griffith@taillight.com>
2022-09-23 11:57:17 -0500

    adds back spelling checking with null-ls

--- lua/config/null-ls.lua
+++ lua/config/null-ls.lua
@@ -2,7 +2,9 @@ local null_ls = require("null-ls")
 
 null_ls.setup({
     sources = {
-        null_ls.builtins.diagnostics.cspell,
+        null_ls.builtins.diagnostics.codespell,
+        null_ls.builtins.formatting.codespell,
         null_ls.builtins.formatting.prettierd,
+        null_ls.builtins.hover.printenv,
     }
 })
