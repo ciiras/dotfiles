3e1c0f9e529780216d540da2f13b6e5e4bce56d8
Christopher Griffith <christopher.griffith@taillight.com>
2023-01-07 13:22:34 -0600

    updates 'all' commands to ignore the last carriage return

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -1,6 +1,5 @@
 nmap('<leader>r', ':source $MYVIMRC<CR> :echo \'---.vimrc reloaded---\'<CR>')
 
--- Buffers {{{
 nmap('<leader>bl', '<cmd>ls<CR>')
 nmap('<leader>bp', '<cmd>bp<CR>')
 nmap('<leader>bn', '<cmd>bn<CR>')
@@ -8,11 +7,9 @@ nmap('<leader>bd', '<cmd>bd<CR>')
 nmap('<leader>bD', '<cmd>%bd<CR>')
 nmap('<leader>bo', '<cmd>on<CR>')
 nmap('<leader>bs', '<cmd>new<CR>')
--- }}}
 
 nmap('<esc>', '<cmd>nohlsearch<CR>')
 
--- Splits {{{
 nmap('<leader>sh', '<C-W>s')
 nmap('<leader>sv', '<C-W>v')
 
@@ -21,13 +18,12 @@ nmap('<M-l>', '<C-W><')
 nmap('<M-j>', '<C-W>-')
 nmap('<M-k>', '<C-W>+')
 nmap('<M-=>', '<C-W>=')
--- }}}
 
 nmap('q:', '<Nop>')
 xmap('p', 'pgvy')
 
-nmap('<leader>da', 'ggvGD')
-nmap('<leader>va', 'ggvG')
-nmap('<leader>ya', 'ggvGY')
+nmap('<leader>da', 'ggvGg_d')
+nmap('<leader>va', 'ggvGg_')
+nmap('<leader>ya', 'ggvGg_y')
 
 nmap('<leader>.', '<cmd>NvimTreeFindFileToggle<CR>')
