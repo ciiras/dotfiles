c1bf2ce9373f3864aa400a70128e4accdd860616
Christopher Griffith <christopher.griffith@taillight.com>
2023-01-17 09:58:37 -0600

    adds java lsp

--- lua/config/nvim-lspconfig.lua
+++ lua/config/nvim-lspconfig.lua
@@ -7,8 +7,9 @@ require('mason').setup({
 
 require('mason-lspconfig').setup({
     ensure_installed = {
+        'jdtls',
+        'sumneko_lua',
         'tsserver',
-        'sumneko_lua'
     }
 })
 
