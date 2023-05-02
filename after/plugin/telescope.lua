f309c544b204f1a3c48d96ebbf9bf27698f73cce
Christopher Griffith <christopher.griffith@taillight.com>
2023-05-02 08:57:18 -0500

    adds telescope key binding to view open buffers

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -1,5 +1,6 @@
+nmap('<leader>fb', '<cmd>lua require("telescope.builtin").buffers()<CR>')
 nmap('<leader>fd', '<cmd>lua require("telescope.builtin").diagnostics()<CR>')
 nmap('<leader>ff', '<cmd>lua require("telescope.builtin").find_files()<CR>')
 nmap('<leader>fg', '<cmd>lua require("modules.telescope.git_status").git_status()<CR>')
 nmap('<leader>fr', '<cmd>lua require("telescope.builtin").live_grep()<CR>')
-nmap('<leader>fb', '<cmd>lua require("telescope").extensions.file_browser.file_browser()<CR>')
+nmap('<leader>fv', '<cmd>lua require("telescope").extensions.file_browser.file_browser()<CR>')
