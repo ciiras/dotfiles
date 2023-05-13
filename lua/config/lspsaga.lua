3b06552c4646cdd1757f4fbf7f0380e164a51446
Christopher Griffith <christopher.griffith@taillight.com>
2023-05-13 10:45:01 -0500

    turns off symbols in winbar

--- lua/config/lspsaga.lua
+++ lua/config/lspsaga.lua
@@ -2,6 +2,9 @@ require("lspsaga").setup({
     server_filetype_map = {
         typescript = 'typescript'
     },
+    symbol_in_winbar = {
+        enable = false,
+    },
     rename = {
         whole_project = false,
     },
