45e13531820714c5495a8667616298ab91710b44
Christopher Griffith <christopher.griffith@taillight.com>
2023-05-15 12:53:31 -0500

    ensures only one window is open when closing all but the current buffer

--- after/plugin/barbar.lua
+++ after/plugin/barbar.lua
@@ -1,3 +1,3 @@
 nmap('<C-p>', '<Cmd>BufferPick<CR>')
-nmap('<leader>bo', '<Cmd>BufferCloseAllButCurrentOrPinned<CR>')
+nmap('<leader>bo', '<Cmd>BufferCloseAllButCurrentOrPinned<CR><Cmd>on<CR>')
 nmap('<leader>bd', '<Cmd>BufferClose<CR>')
