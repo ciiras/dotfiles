9dab133be056f62c029677fea5e8f90c0ce3757d
Christopher Griffith <ciiras@users.noreply.github.com>
2023-05-30 18:55:46 -0500

    removes coc-settings symlink

--- create-symlinks.sh
+++ create-symlinks.sh
@@ -5,10 +5,10 @@ curDir=$(pwd)
 ln -sfF ${curDir}/init.lua ~/.config/nvim/init.lua
 ln -sfF ${curDir}/lua ~/.config/nvim/lua
 ln -sfF ${curDir}/after ~/.config/nvim/after
-ln -sfF ${curDir}/coc-settings.json ~/.config/nvim/coc-settings.json
 
 ln -sfF ${curDir}/.gitconfig ~/.gitconfig
 ln -sfF ${curDir}/.gitignore ~/.gitignore
+
 ln -sfF ${curDir}/.tmux.conf ~/.tmux.conf
 ln -sfF ${curDir}/.zshrc ~/.zshrc
 ln -sfF ${curDir}/kitty.conf ~/.config/kitty/kitty.conf
