0d3e73fac240b89421512919589e1ca9f6641cf0
Christopher Griffith <christopher.griffith@taillight.com>
2023-07-01 14:58:25 -0500

    adds cspell and stylua to null-ls built ins

--- nvim/lua/config/null-ls.lua
+++ nvim/lua/config/null-ls.lua
@@ -1,10 +1,14 @@
 local null_ls = require('null-ls')
 
 null_ls.setup({
+    debug = false,
     sources = {
+        null_ls.builtins.code_actions.cspell,
         null_ls.builtins.code_actions.eslint_d,
+        null_ls.builtins.diagnostics.cspell,
         null_ls.builtins.diagnostics.eslint_d,
         null_ls.builtins.formatting.prettierd,
+        null_ls.builtins.formatting.stylua,
         null_ls.builtins.hover.printenv,
     }
 })
