680c8df093608c8a4bbcdc499e9bd7c7c174b5d6
Christopher Griffith <christopher.griffith@taillight.com>
2023-07-12 13:04:46 -0500

    updates nvim-tree config always open a separate split

--- nvim/lua/config/nvim-tree.lua
+++ nvim/lua/config/nvim-tree.lua
@@ -2,7 +2,7 @@ require('nvim-tree').setup({
     git = {
         ignore = false,
     },
-    hijack_unnamed_buffer_when_opening = true,
+    hijack_unnamed_buffer_when_opening = false,
     view = {
         adaptive_size = true,
     },
