8651e696064ce133516e2556e6def8898beabbb3
Christopher Griffith <christopher.griffith@taillight.com>
2023-08-02 07:52:17 -0600

    updates mini surround key bindings to match vim surround so that the 's' motion no longer has a delay

--- nvim/lua/config/mini-surround.lua
+++ nvim/lua/config/mini-surround.lua
@@ -1,1 +1,11 @@
-require('mini.surround').setup()
+require('mini.surround').setup({
+    mappings = {
+        add = 'S',
+        delete = 'ds',
+        find = '',
+        find_left = '',
+        highlight = '',
+        replace = 'cs',
+        update_n_lines = '',
+      },
+})
