8dc6337203d9b547b3a30569252afa48487b6c95
Christopher Griffith <christopher.griffith@taillight.com>
2023-10-25 14:21:47 -0600

    adds key barbar key bindings for next/prev buffer

--- nvim/after/plugin/barbar.lua
+++ nvim/after/plugin/barbar.lua
@@ -1,3 +1,7 @@
 nmap('<C-p>', '<Cmd>BufferPick<CR>')
+
+nmap('<A-,>', '<Cmd>BufferPrevious<CR>')
+nmap('<A-.>', '<Cmd>BufferNext<CR>')
+
 nmap('<leader>bo', '<Cmd>BufferCloseAllButCurrentOrPinned<CR><Cmd>on<CR>')
 nmap('<leader>bd', '<Cmd>BufferClose<CR>')
