8a3867f4773d98563090a2cd00cbe057b5988f7d
Christopher Griffith <christopher.griffith@taillight.com>
2023-12-28 08:49:29 -0700

    moves cspell.json from ~/ to ~/.config/cspell/

--- nvim/lua/config/null-ls.lua
+++ nvim/lua/config/null-ls.lua
@@ -3,7 +3,13 @@ local null_ls = require('null-ls')
 null_ls.setup({
     debug = false,
     sources = {
-        null_ls.builtins.code_actions.cspell,
+        null_ls.builtins.code_actions.cspell.with({
+            config = {
+                find_json = function(_)
+                    return vim.fn.expand('~/.config/cspell.json')
+                end
+            }
+        }),
         null_ls.builtins.code_actions.eslint_d,
         null_ls.builtins.diagnostics.cspell,
         null_ls.builtins.diagnostics.eslint_d,
--- scripts/symlinks.sh
+++ scripts/symlinks.sh
@@ -2,17 +2,17 @@ USER_DIR=/Users/$USER
 DOT_FILES_DIR=$USER_DIR/projects/dotfiles
 XDG_CONFIG_HOME=$USER_DIR/.config
 
+ln -sfF $DOT_FILES_DIR/cspell.json $XDG_CONFIG_HOME/cspell/cspell.json
 ln -sfF $DOT_FILES_DIR/karabiner $XDG_CONFIG_HOME/karabiner
 ln -sfF $DOT_FILES_DIR/kitty.conf $XDG_CONFIG_HOME/kitty/kitty.conf
 ln -sfF $DOT_FILES_DIR/nvim/after $XDG_CONFIG_HOME/nvim/after
 ln -sfF $DOT_FILES_DIR/nvim/init.lua $XDG_CONFIG_HOME/nvim/init.lua
 ln -sfF $DOT_FILES_DIR/nvim/lua $XDG_CONFIG_HOME/nvim/lua
-ln -sfF $DOT_FILES_DIR/yabai/yabairc $XDG_CONFIG_HOME/yabai/yabairc
 ln -sfF $DOT_FILES_DIR/skhd/skhdrc $XDG_CONFIG_HOME/skhd/skhdrc
+ln -sfF $DOT_FILES_DIR/yabai/yabairc $XDG_CONFIG_HOME/yabai/yabairc
 
 ln -sfF $DOT_FILES_DIR/.amethyst.yml $USER_DIR/.amethyst.yml
 ln -sfF $DOT_FILES_DIR/.gitconfig $USER_DIR/.gitconfig
 ln -sfF $DOT_FILES_DIR/.gitignore $USER_DIR/.gitignore
 ln -sfF $DOT_FILES_DIR/.tmux.conf $USER_DIR/.tmux.conf
 ln -sfF $DOT_FILES_DIR/.zshrc $USER_DIR/.zshrc
-ln -sfF $DOT_FILES_DIR/cspell.json $USER_DIR/cspell.json
