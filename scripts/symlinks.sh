f1340fdab296c96bac4518215e2961bec3de36ef
Christopher Griffith <christopher.griffith@taillight.com>
2024-01-27 09:58:12 -0700

    creates /private/etc/zsh/zshenv that sets ZDOTDIR to XDG spec and moves .zshrc to XDG config dir

--- scripts/symlinks.sh
+++ scripts/symlinks.sh
@@ -3,12 +3,13 @@ TEST_TYPE_SYMLINK=-L
 createSymLink() {
     local source_path=$1
     local dest_path=$2
+    local sudo_cmd=$3
 
     if test $TEST_TYPE_SYMLINK $dest_path; then
         return
     fi
 
-    ln -sfF $source_path $dest_path
+    $sudo_cmd ln -sfF $source_path $dest_path
 }
 
 mkdir -p ~/.config/kitty
@@ -17,6 +18,7 @@ mkdir -p ~/.config/nvim/spell
 mkdir -p ~/.config/lua
 mkdir -p ~/.config/skhd
 mkdir -p ~/.config/yabai
+mkdir -p ~/.config/zsh
 
 createSymLink $DOT_FILES_DIR/karabiner $XDG_CONFIG_HOME/karabiner
 createSymLink $DOT_FILES_DIR/kitty.conf $XDG_CONFIG_HOME/kitty/kitty.conf
@@ -26,9 +28,11 @@ createSymLink $DOT_FILES_DIR/nvim/lua $XDG_CONFIG_HOME/nvim/lua
 createSymLink $DOT_FILES_DIR/nvim/spell/en.utf-8.add $XDG_CONFIG_HOME/nvim/spell/en.utf-8.add
 createSymLink $DOT_FILES_DIR/skhd/skhdrc $XDG_CONFIG_HOME/skhd/skhdrc
 createSymLink $DOT_FILES_DIR/yabai/yabairc $XDG_CONFIG_HOME/yabai/yabairc
+createSymLink $DOT_FILES_DIR/zsh/.zshrc $XDG_CONFIG_HOME/zsh/.zshrc
 
 createSymLink $DOT_FILES_DIR/.amethyst.yml $HOME/.amethyst.yml
 createSymLink $DOT_FILES_DIR/.gitconfig $HOME/.gitconfig
 createSymLink $DOT_FILES_DIR/.gitignore $HOME/.gitignore
 createSymLink $DOT_FILES_DIR/.tmux.conf $HOME/.tmux.conf
-createSymLink $DOT_FILES_DIR/zsh/.zshrc $HOME/.zshrc
+
+createSymLink $DOT_FILES_DIR/zsh/zshenv /private/etc/zshenv sudo
