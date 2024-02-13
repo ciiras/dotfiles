b72932be48a412f309a7b37615c4af4d6e8b8dfe
Christopher Griffith <christopher.griffith@taillight.com>
2024-02-13 15:04:13 -0700

    prevents lsp progess notifcations from appearing via noice

--- nvim/lua/config/noice.lua
+++ nvim/lua/config/noice.lua
@@ -5,9 +5,12 @@ require('noice').setup({
     },
     lsp = {
         override = {
-            ['vim.lsp.util.convert_input_to_markdown_lines'] = true,
-            ['vim.lsp.util.stylize_markdown'] = true,
-            ['cmp.entry.get_documentation'] = true,
+             ['vim.lsp.util.convert_input_to_markdown_lines'] = true,
+             ['vim.lsp.util.stylize_markdown'] = true,
+             ['cmp.entry.get_documentation'] = true,
+        },
+        progress = {
+            enabled = false,
         },
     },
     presets = {
