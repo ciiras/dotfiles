7686cff316e7c04998695f8b246d37a73fa673d2
Christopher Griffith <christopher.griffith@taillight.com>
2024-02-15 11:00:38 -0700

    adds lazy-lock.json

--- scripts/symlinks.sh
+++ scripts/symlinks.sh
@@ -32,6 +32,7 @@ createSymLink $DOT_FILES_DIR/karabiner $XDG_CONFIG_HOME/karabiner
 createSymLink $DOT_FILES_DIR/kitty/kitty.conf $XDG_CONFIG_HOME/kitty/kitty.conf
 createSymLink $DOT_FILES_DIR/nvim/after $XDG_CONFIG_HOME/nvim/after
 createSymLink $DOT_FILES_DIR/nvim/init.lua $XDG_CONFIG_HOME/nvim/init.lua
+createSymLink $DOT_FILES_DIR/nvim/lazy-lock.json $XDG_CONFIG_HOME/nvim/lazy-lock.json
 createSymLink $DOT_FILES_DIR/nvim/lua $XDG_CONFIG_HOME/nvim/lua
 createSymLink $DOT_FILES_DIR/nvim/spell/en.utf-8.add $XDG_CONFIG_HOME/nvim/spell/en.utf-8.add
 createSymLink $DOT_FILES_DIR/skhd/skhdrc $XDG_CONFIG_HOME/skhd/skhdrc
