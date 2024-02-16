e207eade28dd849e3ccfa66ab8e82cb0b7755fcd
Christopher Griffith <christopher.griffith@taillight.com>
2024-02-16 09:24:54 -0700

    updates drag n drop ability for a window to be all of the window, not just the title bar

--- scripts/osx.sh
+++ scripts/osx.sh
@@ -1,7 +1,5 @@
 #!/bin/zsh
 
-curUser=$(whoami)
-
 # Keyboard Repeat
 defaults write -g InitialKeyRepeat -int 15 # normal minimum is 15 (225 ms)
 defaults write -g KeyRepeat -int 1 # normal minimum is 2 (30 ms)
@@ -12,13 +10,16 @@ defaults write com.apple.dock autohide-delay -float 0; defaults write com.apple.
 # UNDO - defaults write com.apple.dock autohide-delay -float 0.5; defaults write com.apple.dock autohide-time-modifier -int 0.5 ;killall Dock
 
 # Screenshots Location
-ssDir="/Users/${curUser}/Pictures"
-mkdir ${ssDir}
-defaults write com.apple.screencapture location ${ssDir}
+screen_short_directory="$HOME/Pictures"
+mkdir -p ${screen_short_directory}
+defaults write com.apple.screencapture location ${screen_short_directory}
 
 # Reduce Display Motion
 defaults write com.apple.Accessibility ReduceMotionEnabled -bool true
-defaults write com.apple.universalaccess reduceMotion -bool true # Having issues with this one
+defaults write com.apple.universalaccess reduceMotion -bool true # terminal executing command must have full disk access
 
 # Disable Disk Ejection Warning
 sudo defaults write /Library/Preferences/SystemConfiguration/com.apple.DiskArbitration.diskarbitrationd.plist DADisableEjectNotification -bool YES && sudo pkill diskarbitrationd
+
+# Ability to drag n drop any part of a window
+defaults write -g NSWindowShouldDragOnGesture -bool true
