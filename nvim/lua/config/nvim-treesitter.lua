2b276f9e42db21c3bbed4eeb56591bb2d2765212
Christopher Griffith <christopher.griffith@taillight.com>
2024-03-05 12:57:33 -0700

    replaces treesitter disable function w/ list of one, 'json' because the function was SLOW

--- nvim/lua/config/nvim-treesitter.lua
+++ nvim/lua/config/nvim-treesitter.lua
@@ -8,17 +8,9 @@ require('nvim-treesitter.configs').setup({ ---@diagnostic disable-line missing-f
     },
     highlight = {
         enable = true,
-        disable = function(lang, buf)
-            local max_filesize = 1000 * 1024 -- 1 MB
-            local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
-            if ok and stats and stats.size > max_filesize then
-                local buffer_name = vim.api.nvim_buf_get_name(buf)
-                print('Disabled treesitter highlighting for', buffer_name, 'with size', stats.size, 'bytes')
-                return true
-            end
-
-            return false
-        end
+        disable = {
+            'json',
+        },
     },
     ignore_install = {},
     query_linter = {
