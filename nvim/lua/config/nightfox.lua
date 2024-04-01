d25d784e8596866450cbced9cd1aec0122566472
Christopher Griffith <christopher.griffith@taillight.com>
2024-04-01 09:02:47 -0600

    updates cursor color to nightfox blue

--- nvim/lua/config/nightfox.lua
+++ nvim/lua/config/nightfox.lua
@@ -8,7 +8,7 @@ require('nightfox').setup({
     },
     groups = {
         all = {
-            Cursor = { bg = '#81B29A' },
+            Cursor = { bg = '#719CD6' },
             CursorColumn = { bg = '#1C1C1C' },
             CursorLine = { bg = '#1C1C1C' },
             Folded = { fg='#719CD6' },
@@ -16,7 +16,7 @@ require('nightfox').setup({
             Search = { fg ='#393B44', bg = '#F4A261' },
             Substitute = { fg ='#FFFFFF' },
             TelescopeBorder = { fg = '#719CD6' },
-            Visual = { fg = '#6E9783' },
+            Visual = { fg = '#86ABDC' },
             WinSeparator = { fg = '#719CD6' },
         },
     },
