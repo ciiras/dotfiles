ffeab3bfee835ac0d992b80d72f132654f8d92c4
Christopher Griffith <christopher.griffith@taillight.com>
2024-04-06 11:12:20 -0600

    updates open git diff key mapping from <leader>gd to <leader>gdo to avoid waiting on <leader>gdc

--- nvim/after/plugin/neogit.lua
+++ nvim/after/plugin/neogit.lua
@@ -1,3 +1,3 @@
-nmap('<leader>gd', '<cmd>:DiffviewOpen<CR>', { desc  = 'Git Diff'})
+nmap('<leader>gdo', '<cmd>:DiffviewOpen<CR>', { desc  = 'Git Diff'})
 nmap('<leader>gdc', '<cmd>:DiffviewClose<CR>', { desc  = 'Git Diff Close'})
 nmap('<leader>gs', '<cmd>:Neogit<CR>', { desc  = 'Git Status'})
