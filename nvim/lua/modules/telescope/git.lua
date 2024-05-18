d2f2902683b34f1e7395fa35723215cd04ea4b05
Christopher Griffith <christopher.griffith@taillight.com>
2024-05-18 11:31:19 -0600

    updates delta theme to custom evolution of diff-so-fancy

--- nvim/lua/modules/telescope/git.lua
+++ nvim/lua/modules/telescope/git.lua
@@ -1,19 +1,26 @@
 local previewers = require('telescope.previewers')
 local builtin = require('telescope.builtin')
-local conf = require('telescope.config')
+
 local M = {}
 
 local delta = previewers.new_termopen_previewer {
-  get_command = function(entry)
-      return { 'git', '-c', 'core.pager=delta --diff-so-fancy', '-c', 'delta.side-by-side=false', 'diff', entry.path }
-  end
+    get_command = function(entry)
+        return {
+            'git',
+            '-c', 'core.pager=delta',
+            '-c', 'delta.side-by-side=false',
+            '-c', 'delta.features=diff-so-fancy-evolution',
+            'diff',
+            entry.path,
+        }
+    end
 }
 
 M.git_status = function(opts)
-  opts = opts or {}
-  opts.previewer = delta
+    opts = opts or {}
+    opts.previewer = delta
 
-  builtin.git_status(opts)
+    builtin.git_status(opts)
 end
 
 return M
