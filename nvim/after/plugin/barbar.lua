93a2be9ec9c2c1344614b690d9745f54798f0477
Christopher Griffith <christopher.griffith@taillight.com>
2024-05-24 08:48:19 -0600

    adds delete all buffers key mapping

--- nvim/after/plugin/barbar.lua
+++ nvim/after/plugin/barbar.lua
@@ -1,7 +1,9 @@
-nmap('<C-p>', '<Cmd>BufferPick<CR>')
+nmap('<C-p>', '<Cmd>BufferPick<CR>', { desc = 'Choose Buffer' })
 
-nmap('<A-,>', '<Cmd>BufferPrevious<CR>')
-nmap('<A-.>', '<Cmd>BufferNext<CR>')
+nmap('<A-,>', '<Cmd>BufferPrevious<CR>', { desc = 'Previous Buffer' })
+nmap('<A-.>', '<Cmd>BufferNext<CR>', { desc = 'Next Buffer' })
 
-nmap('<leader>bo', '<Cmd>BufferCloseAllButCurrentOrPinned<CR><Cmd>on<CR>')
-nmap('<leader>bd', '<Cmd>BufferClose<CR>')
+nmap('<leader>bo', '<Cmd>BufferCloseAllButCurrentOrPinned<CR><Cmd>on<CR>', { desc = 'Close All Buffers but Current' })
+nmap('<leader>bd', '<Cmd>BufferClose<CR>', { desc = 'Close Current Buffer' })
+nmap('<leader>ba', '<Cmd>bufdo confirm bwipeout<CR>', { desc = 'Close All Buffers' })
+
