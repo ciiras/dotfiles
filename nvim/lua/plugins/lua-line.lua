eec82ada8c42b53bcdc13f376c964ab5a6ad86fb
Christopher Griffith <christopher.griffith@taillight.com>
2024-05-26 09:29:58 -0600

    formats lualine config

--- nvim/lua/plugins/lua-line.lua
+++ nvim/lua/plugins/lua-line.lua
@@ -2,7 +2,7 @@ return {
     'nvim-lualine/lualine.nvim',
     config = function()
         local modifiedColors = function()
-           return { fg = '#dfdfe0', bg = vim.bo.modified and '#c94f6d' or '#39506d' }
+            return { fg = '#dfdfe0', bg = vim.bo.modified and '#c94f6d' or '#39506d' }
         end
 
         require('lualine').setup({
@@ -16,14 +16,19 @@ return {
                     {
                         'filename',
                         color = modifiedColors,
-                        path =  3,
+                        path = 3,
                         shorting_target = 10,
                     },
                 },
                 lualine_x = {
                     {
-                      'filetype',
-                      icon_only = true,
+                        'filetype',
+                        icon_only = true,
+                    },
+                    {
+                        require('noice').api.status.command.get,
+                        cond = require('noice').api.status.command.has,
+                        color = { fg = '#FF9E64' },
                     },
                 },
                 lualine_y = {},
