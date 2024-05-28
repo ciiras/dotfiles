545746d29aa54e5acfbc8721b88845053c7c7c08
Christopher Griffith <christopher.griffith@taillight.com>
2024-05-28 12:59:33 -0600

    handles renamed git paths for telescope diff module

--- nvim/lua/modules/telescope/git.lua
+++ nvim/lua/modules/telescope/git.lua
@@ -1,20 +1,44 @@
 local previewers = require('telescope.previewers')
 local builtin = require('telescope.builtin')
 
-local M = {}
+
+local function trim(s)
+    return s:match("^%s*(.-)%s*$")
+end
+
+local function parse_new_path_from_git_rename(str)
+    local result = {}
+    local delimiter = '->'
+    local pattern = '(.-)' .. delimiter
+
+    for part in (str .. delimiter):gmatch(pattern) do
+      if part ~= "" then
+        table.insert(result, part)
+      end
+    end
+
+  return trim(result[#result])
+end
 
 local delta = previewers.new_termopen_previewer {
     get_command = function(entry)
+        local path = entry.path
+        if entry.status == 'RM' then -- Renamed file format: old path -> new path
+            path = parse_new_path_from_git_rename(entry.path)
+        end
+
         return {
             'git',
             '-c', 'core.pager=delta',
             '-c', 'delta.side-by-side=false',
             '-c', 'delta.features=diff-so-fancy-evolution',
             'diff',
-            entry.path,
+            path,
         }
     end
 }
+
+local M = {}
 
 M.git_status = function(opts)
     opts = opts or {}
