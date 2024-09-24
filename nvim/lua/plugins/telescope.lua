2c8f4a8feb11bdda86415c7ba7e1da238a5207d7
Christopher Griffith <christopher.griffith@taillight.com>
2024-09-24 13:36:59 -0600

    overrides telescopes enter key with select one or multiple logic

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -1,3 +1,20 @@
+local select_one_or_multi = function(prompt_bufnr)
+    local actions = require('telescope.actions')
+    local picker = require('telescope.actions.state').get_current_picker(prompt_bufnr)
+
+    local multi = picker:get_multi_selection()
+    if not vim.tbl_isempty(multi) then
+        actions.close(prompt_bufnr)
+        for _, j in pairs(multi) do
+            if j.path ~= nil then
+                vim.cmd(string.format('%s %s', 'edit', j.path))
+            end
+    end
+    else
+        actions.select_default(prompt_bufnr)
+    end
+end
+
 return {
     'nvim-telescope/telescope.nvim',
     dependencies = {
@@ -36,6 +53,7 @@ return {
                 mappings = {
                     i = {
                         ['<ESC>'] = actions.close,
+                        ['<CR>'] = select_one_or_multi,
 
                         ['<C-N>'] = actions.move_selection_next,
                         ['<C-P>'] = actions.move_selection_previous,
