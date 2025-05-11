ed2e2c0e803e9bb025059e500d5105135d6c1665
Christopher Griffith <christopher.griffith@taillight.com>
2025-05-11 10:59:30 -0600

    changes vimatrix delay from 1 to 3 minutes

--- nvim/lua/plugins/vimatrix.lua
+++ nvim/lua/plugins/vimatrix.lua
@@ -5,7 +5,7 @@ return {
         auto_activation = {
             screensaver = {
                 setup_deferral = 0,
-                timeout = 60,
+                timeout = 180,
                 ignore_focus = true,
                 block_on_term = true,
                 block_on_cmd_line = true,
