bce3a0bd01aa252a9b144d7b5e57e84a3e314510
Christopher Griffith <christopher.griffith@taillight.com>
2025-07-30 08:33:03 -0600

    removes neogit key mappings

--- nvim/after/plugin/neogit.lua
+++
@@ -1,3 +0,0 @@
-nmap('<leader>gdo', '<cmd>:DiffviewOpen<CR>', { desc  = 'Git Diff'})
-nmap('<leader>gdc', '<cmd>:DiffviewClose<CR>', { desc  = 'Git Diff Close'})
-nmap('<leader>gs', '<cmd>:Neogit<CR>', { desc  = 'Git Status'})
