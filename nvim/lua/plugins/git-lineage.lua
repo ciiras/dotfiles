26fb42b8930c02b73c123956a186362857b45766
Christopher Griffith <christopher.griffith@taillight.com>
2026-02-05 07:52:34 -0700

    adds gitlineage nvim

---
+++ nvim/lua/plugins/git-lineage.lua
@@ -0,0 +1,10 @@
+return {
+    'lionyxml/gitlineage.nvim',
+    dependencies = {
+        'sindrets/diffview.nvim',
+    },
+    config = function()
+        require('gitlineage').setup()
+    end,
+    event = 'VeryLazy',
+}
