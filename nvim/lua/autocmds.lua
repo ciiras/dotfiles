9d937fdea351af14d78e1ed14ef4097517c28f44
Christopher Griffith <christopher.griffith@taillight.com>
2026-04-22 15:01:06 -0500

    adds line-justice.nvim plugin to have both absolute and relative line numbers at all times

--- nvim/lua/autocmds.lua
+++ nvim/lua/autocmds.lua
@@ -4,34 +4,7 @@ function! StripTrailingWhitespace()
     %s/\s\+$//e
 endfunction
 
-function! IsExcludedFileType()
-    if &ft =~ 'NvimTree'
-        return 'excluded'
-    elseif &ft =~ 'sagarename'
-        return 'excluded'
-    endif
-endfunction
-
-function! TurnOnRelativeNumber()
-    if IsExcludedFileType() =~ 'excluded' | return | endif
-    set number relativenumber
-endfunction
-
-function! TurnOffRelativeNumber()
-    if IsExcludedFileType() =~ 'excluded'| return | endif
-    set number norelativenumber
-endfunction
 ]], false)
-
-local turn_on_relative_number = function()
-    vim.api.nvim_call_function('TurnOnRelativeNumber', {})
-end
-create_autocmd({ 'BufEnter', 'FocusGained', 'InsertLeave' }, { callback = turn_on_relative_number })
-
-local turn_off_relative_number = function()
-    vim.api.nvim_call_function('TurnOffRelativeNumber', {})
-end
-create_autocmd({ 'BufLeave', 'FocusLost', 'InsertEnter' }, { callback = turn_off_relative_number })
 
 create_autocmd({ 'BufRead' }, { command = 'setlocal foldmethod=marker' })
 
